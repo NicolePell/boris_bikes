656d9210d129b1ff0cf4b03d779aefdb7161b422
Nicole Pellicena Tredway <pellicena@gmail.com>
2014-09-15 20:10:40 +0100

    Make bike method work

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -10,6 +10,11 @@ describe Bike do
     # expect an instance of this class to have 
     # a method "broken?" that should return false
     expect(the_bike).not_to be_broken
-  end	
+  end
 
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break!
+    expect(bike).to be_broken
+  end
 end
