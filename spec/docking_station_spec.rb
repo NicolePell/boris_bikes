bc0339ca6585f0bf57eae77fa0caab60ecf013c2
EllaNancyFay <ellaschofield0@gmail.com>
2014-09-16 18:10:51 +0100

    update require file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
-require '../lib/docking_station'
-require '../lib/bike'
+require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
 	let(:bike) {Bike.new}
-	let(:station) {DockingStation.new} 
+	let(:station) { DockingStation.new(:capacity =>20) } 
 
 	it "should accept a bike" do
 		expect(station.bike_count).to eq(0)
@@ -16,5 +16,11 @@ describe DockingStation do
 		station.dock(bike)
 		station.release(bike)
 		expect(station.bike_count).to eq(0)
+	end
+
+	it "should know when it's full" do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full
 	end
 end
