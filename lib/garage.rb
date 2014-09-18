1aa6189c7870dedb4d8ccd2f442ab26ced95c9d5
EllaNancyFay <ellaschofield0@gmail.com>
2014-09-18 17:33:30 +0100

    Garage can fix bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -4,16 +4,17 @@ class Garage
 
 	include BikeContainer
 
-	def accept(bike)
-		bike.fix!
-		#dock(bike)
-	end
-
 		def dock_broken(van)
-		van.broken_bikes.each do |bike|
+			van.broken_bikes.each do |bike|	
 			dock(bike)
 			van.release_broken
-		end 
-	end
+			end 
+		end
+
+		def fix_bike
+			broken_bikes.each do |bike|
+			bike.fix!
+			end
+		end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -8,13 +8,6 @@ describe Garage do
 	let(:garage) { Garage.new } 
 	let(:van) { Van.new }
 
-	it "should fix broken bikes" do
-		bike.break!
-		bike.fix!
-		garage.dock(bike)
-		expect(bike).to_not be_broken
-	end
-
 	it "should accept broken bikes from the van" do
 		working_bike, broken_bike, broken_bike2 = Bike.new, Bike.new, Bike.new
 		broken_bike.break!
@@ -26,4 +19,12 @@ describe Garage do
 		expect(garage.broken_bikes.count).to eq(2)
 	end
 
+	it "should fix docked bikes" do
+		broken_bike = Bike.new
+		broken_bike.break!
+		van.dock(broken_bike)
+		garage.dock_broken(van)
+		garage.fix_bike
+		expect(garage.available_bikes.count).to eq(1)
+	end
 end
